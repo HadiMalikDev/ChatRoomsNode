e4f0ee8820fe8eaca9c1784aeff1b5909d57d0fd
Muhammad Hadi <hadimalikdev@gmail.com>
2022-05-16 12:20:24 +0500

    Added default handling

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const app = express();
+const router = express.Router();
 //Security
 app.use(helmet());
 app.use(cors());
@@ -24,14 +25,30 @@ app.use(express.json());
 //Routers
 app.use(userRouter);
 app.use(roomRouter);
-
+router.get("/", (req, res) => {
+  return res.redirect("/users/login");
+});
+app.use(router);
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(function (req, res) {
+  // Invalid request
+  res.json({
+    error: {
+      name: "Error",
+      status: 404,
+      message: "Invalid Request",
+      statusCode: 404,
+    },
+    message: "Testing!",
+  });
+});
+
 console.log(path.join(__dirname, "../public"));
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const startApplication = async () => {
-  await connect(process.env.MONGO_URI)
+  await connect(process.env.MONGO_URI);
   socketServer(server);
   server.listen(PORT, () => {
     console.log("Server up");
